d6fee455ca20f06ae692acb52610ddc6378fecc8
Vasyl Miskiv <vasylmiskiv@gmail.com>
2023-02-02 01:52:47 +0200

    some fixes

--- client/src/service/socket.ts
+++ client/src/service/socket.ts
@@ -1,3 +1,3 @@
 import * as io from "socket.io-client";
 
-export const socket = io.connect(`http://192.168.0.100:5000`);
+export const socket = io.connect(`http://vmiskivchatsocketio.onrender.com/`);
